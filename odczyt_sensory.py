ec5a1886eb66a891197848827b4d6c772270819f
Niezniszczalnik <130781376+Niezniszczalnik@users.noreply.github.com>
2025-06-08 12:58:11 +0200

    Update odczyt_sensory.py

--- odczyt_sensory.py
+++ odczyt_sensory.py
@@ -1,7 +1,18 @@
 """Prosty program uruchamiajacy serwer z danymi z sensorow"""
 # Plik wejsciowy uruchamiany na Raspberry Pi
+import warnings
 import asyncio
 from sensor_server import SensorServer
+
+# Adafruit Blinka on Linux tries to set the I2C frequency which is not
+# supported. This produces a noisy RuntimeWarning. We silence that specific
+# warning to avoid confusing users.
+warnings.filterwarnings(
+    "ignore",
+    message="I2C frequency is not settable in python, ignoring!",
+    category=RuntimeWarning,
+    module="adafruit_blinka.microcontroller.generic_linux.i2c",
+)
 
 if __name__ == "__main__":
     # Tworzymy serwer i uruchamiamy petle glowna
